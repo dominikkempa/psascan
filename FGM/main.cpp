84e35ae6318133de8d226218bf1d2509ab0b7db1
Dominik Kempa <dominik.kempa@gmail.com>
2014-01-04 14:56:17 +0200

    Corrected the usage msg

--- FGM/main.cpp
+++ FGM/main.cpp
@@ -5,8 +5,8 @@
 
 int main(int argc, char **argv) {
   if (argc != 3) {
-    fprintf(stderr, "usage: %s <file> <mem>\n"
-                    "Compute SA of <file> using <mem> MiB of RAM and write to <file>.sa.\n",
+    fprintf(stderr, "usage: %s <file> <mem>\n\n"
+                    "Compute SA of <file> using <mem> MiB of RAM and write to <file>.sa5\n",
                     argv[0]);
     std::exit(EXIT_FAILURE);
   }
