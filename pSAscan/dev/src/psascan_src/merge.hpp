f8429ff311e34d8191c29eb3497c902615869ca2
Dominik Kempa <dominik.kempa@gmail.com>
2017-08-17 03:04:37 +0300

    Update vbyte stream reader

--- pSAscan/dev/src/psascan_src/merge.hpp
+++ pSAscan/dev/src/psascan_src/merge.hpp
@@ -1,4 +1,4 @@
-/**
+/*
  * @file    src/psascan_src/merge.hpp
  * @section LICENCE
  *
@@ -45,7 +45,7 @@
 #include "half_block_info.hpp"
 #include "io/scatterfile.hpp"
 #include "io/async_stream_writer.hpp"
-#include "io/async_vbyte_stream_reader.hpp"
+#include "io/async_stream_vbyte_reader.hpp"
 #include "io/async_scatterfile_reader.hpp"
 
 
@@ -55,8 +55,8 @@ namespace psascan_private {
 template<typename text_offset_type, typename block_offset_type>
 void merge(std::string output_filename, std::uint64_t ram_use,
     std::vector<half_block_info<block_offset_type> > &hblock_info) {
-  std::int64_t n_block = hblock_info.size();
-  std::int64_t text_length = 0;
+  std::uint64_t n_block = hblock_info.size();
+  std::uint64_t text_length = 0;
 
   std::sort(hblock_info.begin(), hblock_info.end());
   for (std::uint64_t j = 0; j < hblock_info.size(); ++j)
@@ -71,7 +71,7 @@ void merge(std::string output_filename, std::uint64_t ram_use,
       sizeof(block_offset_type) * buffer_size,
       (1.L * sizeof(block_offset_type) * buffer_size) / (1 << 20));
 
-  typedef async_vbyte_stream_reader<std::int64_t> vbyte_reader_type;
+  typedef async_stream_vbyte_reader vbyte_reader_type;
   typedef async_stream_writer<text_offset_type> output_writer_type;
   typedef async_scatterfile_reader<block_offset_type> psa_reader_type;
 
@@ -80,47 +80,50 @@ void merge(std::string output_filename, std::uint64_t ram_use,
       sizeof(text_offset_type) * buffer_size, 4UL, "w");
   vbyte_reader_type **gap = new vbyte_reader_type*[n_block - 1];
 
-  for (std::int64_t i = 0; i < n_block; ++i) {
+  for (std::uint64_t i = 0; i < n_block; ++i) {
     psa_readers[i] = new psa_reader_type(&hblock_info[i].psa,
         buffer_size * sizeof(block_offset_type));
+
     if (i + 1 != n_block)
-      gap[i] = new vbyte_reader_type(hblock_info[i].gap_filename,
-          0, buffer_size);
+      gap[i] = new vbyte_reader_type(
+          hblock_info[i].gap_filename, buffer_size,
+          std::max(4UL, buffer_size / (2UL << 20)));
   }
 
-  long *gap_head = new long[n_block];
-  for (long i = 0; i + 1 < n_block; ++i)
+  std::uint64_t *gap_head = new std::uint64_t[n_block];
+  for (std::uint64_t i = 0; i + 1 < n_block; ++i)
     gap_head[i] = gap[i]->read();
   gap_head[n_block - 1] = 0;
 
-  long tmp = (long)sqrtl((long double)n_block);
-  long sblock_size = 1L;
-  long sblock_size_log = 0;
+  std::uint64_t tmp = sqrtl((long double)n_block);
+  std::uint64_t sblock_size = 1L;
+  std::uint64_t sblock_size_log = 0;
   while (sblock_size * 2L <= tmp) {
     sblock_size *= 2L;
     ++sblock_size_log;
   }
 
-  long n_sblocks = (n_block + sblock_size - 1) / sblock_size;
-  std::pair<long, long> *sblock_info = new std::pair<long, long>[n_sblocks];
+  std::uint64_t n_sblocks = (n_block + sblock_size - 1) / sblock_size;
+  std::pair<std::uint64_t, std::uint64_t> *sblock_info =
+    new std::pair<std::uint64_t, std::uint64_t>[n_sblocks];
 
-  for (long i = 0; i < n_sblocks; ++i) {
-    long sblock_beg = i * sblock_size;
-    long sblock_end = std::min(n_block, sblock_beg + sblock_size);
+  for (std::uint64_t i = 0; i < n_sblocks; ++i) {
+    std::uint64_t sblock_beg = i * sblock_size;
+    std::uint64_t sblock_end = std::min(n_block, sblock_beg + sblock_size);
 
     sblock_info[i].second = 0;
     sblock_info[i].first = gap_head[sblock_beg];
-    for (long j = sblock_beg + 1; j < sblock_end; ++j)
+    for (std::uint64_t j = sblock_beg + 1; j < sblock_end; ++j)
       sblock_info[i].first = std::min(sblock_info[i].first, gap_head[j]);
   }
 
   long double merge_start = utils::wclock();
-  for (long i = 0, dbg = 0; i < text_length; ++i, ++dbg) {
+  for (std::uint64_t i = 0, dbg = 0; i < text_length; ++i, ++dbg) {
     if (dbg == (1 << 23)) {
       long double elapsed = utils::wclock() - merge_start;
-      long inp_vol = (1L + sizeof(block_offset_type)) * i;
-      long out_vol = sizeof(text_offset_type) * i;
-      long tot_vol = inp_vol + out_vol;
+      std::uint64_t inp_vol = (1L + sizeof(block_offset_type)) * i;
+      std::uint64_t out_vol = sizeof(text_offset_type) * i;
+      std::uint64_t tot_vol = inp_vol + out_vol;
       long double tot_vol_m = tot_vol / (1024.L * 1024);
       long double io_speed = tot_vol_m / elapsed;
       fprintf(stderr, "\r  %.1Lf%%. Time = %.2Lfs. I/O: %2.LfMiB/s",
@@ -129,7 +132,7 @@ void merge(std::string output_filename, std::uint64_t ram_use,
     }
 
     // Find the superblock containing gap head equal to zero.
-    long k = 0;
+    std::uint64_t k = 0;
     while (sblock_info[k].first != 0) {
       sblock_info[k].first--;
       sblock_info[k].second++;
@@ -137,18 +140,20 @@ void merge(std::string output_filename, std::uint64_t ram_use,
     }
 
     // Find the block with the gap head equal to zero.
-    long sblock_beg = (k << sblock_size_log);
-    long sblock_end = std::min(n_block, sblock_beg + sblock_size);
+    std::uint64_t sblock_beg = (k << sblock_size_log);
+    std::uint64_t sblock_end = std::min(n_block, sblock_beg + sblock_size);
 
-    long new_min = text_length;
-    long j = sblock_beg;
+    std::uint64_t new_min = text_length;
+    std::uint64_t j = sblock_beg;
     while (gap_head[j] != sblock_info[k].second) {
       gap_head[j] -= (sblock_info[k].second + 1);
       new_min = std::min(new_min, gap_head[j]);
       ++j;
     }
 
-    long SA_i = (long)psa_readers[j]->read() + (long)hblock_info[j].beg;
+    std::uint64_t SA_i =
+      (std::uint64_t)psa_readers[j]->read() +
+      (std::uint64_t)hblock_info[j].beg;
 
     if (j != n_block - 1) gap_head[j] = gap[j]->read();
     new_min = std::min(new_min, gap_head[j]);
@@ -166,6 +171,10 @@ void merge(std::string output_filename, std::uint64_t ram_use,
     output->write(SA_i);
   }
 
+  // Stop I/O threads.
+  for (std::uint64_t i = 0; i + 1 < n_block; ++i)
+    gap[i]->stop_reading();
+
   // Update I/O volume.
   std::uint64_t io_volume =
     (1 + sizeof(block_offset_type) + sizeof(text_offset_type)) * text_length;
@@ -177,7 +186,7 @@ void merge(std::string output_filename, std::uint64_t ram_use,
 
   // Clean up.
   delete output;
-  for (long i = 0; i < n_block; ++i) {
+  for (std::uint64_t i = 0; i < n_block; ++i) {
     delete psa_readers[i];
     if (i + 1 != n_block)
       delete gap[i];
@@ -188,7 +197,7 @@ void merge(std::string output_filename, std::uint64_t ram_use,
   delete[] gap_head;
   delete[] sblock_info;
   
-  for (int i = 0; i + 1 < n_block; ++i)
+  for (std::uint64_t i = 0; i + 1 < n_block; ++i)
     utils::file_delete(hblock_info[i].gap_filename);
 }
 
