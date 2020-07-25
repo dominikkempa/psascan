1b3e37c568a85a9d200ffe2527ba8c494f79b8f6
Dominik Kempa <dominik.kempa@gmail.com>
2020-07-25 01:02:28 -0700

    Move dev directory to separate branch

--- dev/tests/smaller-suffixes/smaller_suffixes.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SMALLER_SUFFIXES_H_INCLUDED
-#define __SMALLER_SUFFIXES_H_INCLUDED
-
-#include <string>
-
-long parallel_smaller_suffixes(unsigned char *block, long block_size,
-    std::string text_filename, long suffix_start_pos);
-
-#endif // __SMALLER_SUFFIXES_H_INCLUDED
